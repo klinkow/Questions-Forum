0a2cbf60dd4605a21d2859bae7b90602c362dda9
Bill Klinkow <klinkow@gmail.com>
2017-01-17 09:45:47 -0800

    add functioning answers page without delete

--- app/models/question.js
+++ app/models/question.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   title: DS.attr(),
-  author: DS.attr()
+  author: DS.attr(),
+  answers: DS.hasMany('answer', { async: true })
 });
--- app/routes/question.js
+++ app/routes/question.js
@@ -4,4 +4,24 @@ export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('question', params.question_id);
   },
+
+  // model(params) {
+  //   return Ember.RSVP.hash({
+  //     question: this.store.findRecord('question', params.question_id),
+  //     questions: this.store.findAll('question'),
+  //     answers: this.store.findAll('answer'),
+  //   });
+  // },
+
+  actions: {
+    saveAnswer(params) {
+      var newAnswer = this.store.createRecord('answer', params);
+      var question = params.question;
+      question.get('answers').addObject(newAnswer);
+      newAnswer.save().then(function() {
+        return question.save();
+      });
+      this.transitionTo('question', question);
+    }
+  }
 });
